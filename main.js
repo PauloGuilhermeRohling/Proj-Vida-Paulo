0af716c037a9567929dbe42ce5dcf6b32033265e
PauloGuilhermeRohling <108003299+PauloGuilhermeRohling@users.noreply.github.com>
2024-04-19 10:43:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 
-for (let i=0; i<botoes.length; i++) {
-    botoes [i].onclick = function(){
+for (let i=0; i<botoes.length; i++)  {
+    botoes[i].onclick = function() {
 
         for (let j=0; j<botoes.length; j++) {
             botoes[j].clasList.remove("ativo");
@@ -11,5 +11,26 @@ for (let i=0; i<botoes.length; i++) {
         }
         
         botoes[i].clasList.add("ativo"); 
+        textos[i].clasList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-07-05T00:00:00");
+const tempoAtual = new Date();
+
+contadores[0].textContent = calculaTempo (tempoObjetivo1);
+
+function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %=  60;
+    horas %= 24;
+    
 }
